4218c9246bc7d21d8970a3ad0467af58433bf457
JiCodes <liu.ji1@northeastern.edu>
2024-01-13 15:13:50 -0800

    add new route and pages

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def greet():
-    if request.method == 'POST':
-      name = request.form.get('name')
-      return render_template('greet.html', name=name)
+SPORTS = [
+  'Baseball',
+  'Frisbee',
+  'Soccer'
+]
+
+# @app.route('/', methods=['GET', 'POST'])
+# def greet():
+#     if request.method == 'POST':
+#       name = request.form.get('name')
+#       return render_template('greet.html', name=name)
     
-    return render_template('index.html')
+#     return render_template('index.html')
+
+@app.route('/')
+def index():
+  return render_template('index.html', sports=SPORTS)
+
+@app.route('/register', methods=['POST'])
+def register():
+  if not request.form.get('name') or request.form.get('sport') not in SPORTS:
+    return render_template('failure.html')
+  return render_template('success.html')
