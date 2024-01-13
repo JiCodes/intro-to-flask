6813c8eb837c94a0f43a1a036e5524c4439615e6
JiCodes <liu.ji1@northeastern.edu>
2024-01-13 14:37:34 -0800

    add multiple method to one path

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/greet')
+@app.route('/', methods=['GET', 'POST'])
 def greet():
-    name = request.args.get('name', 'world')
-    return render_template('greet.html', name=name)
+    if request.method == 'POST':
+      name = request.form.get('name')
+      return render_template('greet.html', name=name)
+    
+    return render_template('index.html')
